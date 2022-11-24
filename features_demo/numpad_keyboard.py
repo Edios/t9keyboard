11ab86030530ef84a9fea05e04f56d01721461cb
Edios <edios122@gmail.com>
2022-11-24 10:07:50 +0100

    Cleanup old POC code from file

--- features_demo/numpad_keyboard.py
+++ features_demo/numpad_keyboard.py
@@ -1,136 +1,10 @@
-"""
-# from pynput.keyboard import Key, Listener
-#
-# def on_press(key):
-#     print('{0} pressed'.format(
-#         key))
-#
-# def on_release(key):
-#     print('{0} release'.format(
-#         4862139784key))
-#     if key == Key.esc:
-#         # Stop listener
-#
-#         return False
-#
-# # Collect events until released
-# with Listener(
-#         on_press=on_press,
-#         on_release=on_release) as listener:
-#     listener.join()
-
-#######################Diffrent libary -sucks for debugging
-# import keyboard
-# import time
-# keyboard.start_recording()
-# time.sleep(7)
-# events = keyboard.stop_recording()
-# keyboard.replay(events)
-
-
-
-
-"""
 import copy
 import time
-# Counter of pressed key
-
-# import tkinter as tk
-#
-# root = tk.Tk()
-#
-#
-# def triggered_method(param):
-#     print(f"++++++++ {param} ++++++++++++")
-#
-#
-# def key(event):
-#     print(repr(event.char), repr(event.keysym), repr(event.keycode))
-#     if event.char=="KP_4":
-#         triggered_method("KP_4")
-# root.bind("<Key>", key)
-# root.mainloop()
-
-import tkinter as tk
+#import tkinter as tk
+#from tkinter import ttk
 from dataclasses import dataclass, field
 from enum import Enum
-from tkinter import ttk
 from typing import Union, List
-
-"""
-/*-7894561230,+
-
-Num_Lock
-KP_Divide
-KP_Multiply
-KP_Subtract
-KP_Home
-Num_Lock
-KP_7
-KP_8
-KP_9
-KP_4
-KP_5
-KP_6
-KP_1
-KP_2
-KP_3
-KP_0
-KP_Separator
-KP_Enter
-KP_Add
-Control_L
-
-"""
-
-
-#
-# events_list = []
-# typed=[]
-#
-# def trigger_checking_sequence(listChar):
-#     """
-#     Determine if its double click
-#     :param listChar:
-#     :return:
-#     """
-#     print(f"Sequence {'|'.join(listChar)}")
-#
-#     #Typed is output display
-#     if listChar[-1] ==listChar[-2]:
-#         typed.append(listChar[-2])
-#
-#     if all(x == listChar[0] for x in listChar) and not listChar.__len__()==0:
-#         print("Its doubleclick")
-#     else:
-#         print("Its not doubleclick")
-#
-#
-# def add_to_event_list(input: str, target: list):
-#     print(input)
-#     #target.append(input)
-#     global events_list
-#     events_list.append(input)
-#     trigger_checking_sequence(events_list)
-#     if target.__len__()>2:
-#         events_list.clear()
-#
-#
-# root = tk.Tk()
-# e = ttk.Entry(root)
-# e.place(x=1, y=1)
-# e.bind('<Key>', lambda ev: add_to_event_list(ev.keysym, events_list))
-# root.mainloop()
-
-class Key:
-    values: list[str]
-
-
-@dataclass
-class LastKey:
-    click_time: int
-    value: str
-
 
 class KeypadKey(Enum):
     One = 1
@@ -146,38 +20,6 @@ class KeypadKey(Enum):
 
     def __repr__(self):
         return str(self.value)
-
-
-T9 = {'.': KeypadKey.Seven, ',': KeypadKey.Seven, '!': KeypadKey.Seven, '?': KeypadKey.Seven,
-      ':': KeypadKey.Seven, '-': KeypadKey.Seven, '_': KeypadKey.Seven, '\'': KeypadKey.Seven, '/': KeypadKey.Seven,
-      '*': KeypadKey.Seven,
-      '\\': KeypadKey.Seven, '(': KeypadKey.Seven, ')': KeypadKey.Seven, '<': KeypadKey.Seven, '>': KeypadKey.Seven,
-      ';': KeypadKey.Seven, '[': KeypadKey.Seven, ']': KeypadKey.Seven,
-
-      'a': KeypadKey.Eight, 'b': KeypadKey.Eight, 'c': KeypadKey.Eight,
-
-      'd': KeypadKey.Nine, 'e': KeypadKey.Nine, 'f': KeypadKey.Nine,
-
-      'g': KeypadKey.Four, 'h': KeypadKey.Four, 'i': KeypadKey.Four,
-
-      'j': KeypadKey.Five, 'k': KeypadKey.Five, 'l': KeypadKey.Five,
-
-      'm': KeypadKey.Six, 'n': KeypadKey.Six, 'o': KeypadKey.Six,
-
-      'p': KeypadKey.One, 'q': KeypadKey.One, 'r': KeypadKey.One, 's': KeypadKey.One,
-
-      't': KeypadKey.Two, 'u': KeypadKey.Two, 'v': KeypadKey.Two,
-
-      'w': KeypadKey.Three, 'x': KeypadKey.Three, 'y': KeypadKey.Three, 'z': KeypadKey.Three,
-
-      '0': KeypadKey.Zero,
-
-      '1': KeypadKey.One, '2': KeypadKey.Two, '3': KeypadKey.Three,
-
-      '4': KeypadKey.Four, '5': KeypadKey.Five, '6': KeypadKey.Six, '7': 7,
-
-      '8': KeypadKey.Eight, '9': KeypadKey.Nine
-      }
 
 # TODO: Print this graphics as helper
 """
