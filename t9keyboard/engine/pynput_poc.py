a6bce628c132f7bf3fcb293e0f3612a1a11a600b
Miroslaw <miroslaw.lazarewicz@gmail.com>
2023-01-21 18:17:11 +0100

    Handle method

--- t9keyboard/engine/pynput_poc.py
+++ t9keyboard/engine/pynput_poc.py
@@ -1,25 +1,26 @@
+from typing import Union
+
 from pynput import keyboard
 from pynput.keyboard import KeyCode
 
 from t9keyboard.keyboard_keymap import virtual_key_to_alphabet_keys_map, virtual_key_to_special_keys_map
 
 
-def is_numpad_key(vk_code:int)->bool:
+def is_numpad_key(vk_code:Union[int,str])->bool:
     """
-    Check if given key is Numeric Keypad keyboard key by comparing his vk param.
+    Determine if given input is numeric keypad corresponding virtual-key code.
     Use key map key's which are Virtual-Key Codes.
     """
     numpad_virtual_key_codes=[int(key_code) for key_code in list(virtual_key_to_alphabet_keys_map.keys())+list(virtual_key_to_special_keys_map.keys())]
     return True if vk_code in numpad_virtual_key_codes else False
-def keyboard_listener():
+def keyboard_listener(handled_method=None):
     global key
     global keyboard_listener
 
     def on_press(key):
         if hasattr(key, 'vk') and is_numpad_key(key.vk):
-            print('You entered a number from the numpad: ', key.vk)
-        else:
-            print('normal key', key)
+            if handled_method:
+                handled_method(key)
 
     def on_release(key):
         print('on release', key)
@@ -27,7 +28,6 @@ def keyboard_listener():
             return False
 
     def win32_event_filter(_, data):
-        #if(msg == 257 or msg == 256) and (data.vkCode == 38 or data.vkCode == 40):
         if hasattr(data, 'vkCode') and is_numpad_key(data.vkCode):
             listener._suppress = True
         else:
@@ -35,16 +35,17 @@ def keyboard_listener():
         return True
 
     #it suppress only numpad
-    return  keyboard.Listener(
+    return keyboard.Listener(
         on_press=on_press,
         on_release=on_release,
         win32_event_filter=win32_event_filter,
         suppress=True
     )
 
-
-listener = keyboard_listener()
-
-if __name__== '__main__':
+def method_to_handle(key:KeyCode):
+    print(f"Method got: {key.vk} {key}")
+def numpad_listner():
+    global listener
+    listener = keyboard_listener(handled_method=method_to_handle)
     with listener as ml:
         ml.join()
